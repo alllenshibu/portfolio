7d2120d1ec357466f64f87491cea1fba9ed1ec68
Allen Shibu <93600615+alapanoski@users.noreply.github.com>
2023-05-04 22:39:07 +0530

    Initial deployment

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -36,11 +36,11 @@ const Projects = () => {
     //     <Project focus="right" />
     //   </div>
     // </div>
-    <div className="min-h-[60vh] max-h-[90vh] overflow-hidden flex flex-col justify-center items-start md:items-center">
-      <div className="flex justify-center items-center">
+    <div className="min-h-[60vh] flex flex-col justify-start items-center md:items-center">
+      <div className="pb-10 flex justify-center items-center">
         <h2>Projects</h2>
       </div>
-      <div className="p-8 h-full w-full flex flex-row justify-center items-center flex-wrap gap-8">
+      <div className="p-8 max-h-[70vh] md:max-h-none overflow-scroll md:overflow-visible w-full flex flex-row justify-center items-center flex-wrap gap-8">
         {projects.map((project, index) => (
           <div className="p-5 h-72 w-72 rounded-md border border-gray-200 shadow flex flex-col justify-between hover:bg-gray-50 hover:shadow-lg transition duration-100 ease-in-out">
             <h3 className="z-10 text-xl font-bold">{project.title}</h3>
